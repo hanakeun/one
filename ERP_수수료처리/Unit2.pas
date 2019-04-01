b68fabd1e4c67bbadbb4f41d0161e5dd8b13f877
hanakeun <40451563+hanakeun@users.noreply.github.com>
2019-04-01 16:30:15 +0900

    다시 원상복구!

--- ERP_수수료처리/Unit2.pas
+++ ERP_수수료처리/Unit2.pas
@@ -20,9 +20,6 @@ type
       Shift: TShiftState);
     procedure f2btnExcelClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
-//dlqjsdps durle kdmaxmxmxmxmdkamkmdslkmf
-//gksrmffh TmslRk ahfmrpTwl dlrjf gotjrgkrh dlTekaus sjsms gkfdlfdl rjqsk djqtsmsrjtdlek.
-//akwwl?
 
   private
     { Private declarations }
